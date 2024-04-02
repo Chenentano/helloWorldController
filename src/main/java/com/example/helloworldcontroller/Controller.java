cb94c59259b2c1a3cb47f86e2583875944e9c00a
basti <bastian_behrendt@yahoo.de>
2024-04-02 14:47:26 +0100

    added a PathVariable for the hello method.

--- src/main/java/com/example/helloworldcontroller/Controller.java
+++ src/main/java/com/example/helloworldcontroller/Controller.java
@@ -1,14 +1,17 @@
 package com.example.helloworldcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @RestController
 public class Controller {
 
-    @GetMapping("api/hello")
-    public String hello(){
-        return "Hello World!";
+    @GetMapping("api/hello/{name}")
+    public String hello(@PathVariable String name){
+        return "Hallo " + name;
     }
 
 }
